eb4a27f64cc6b1e48cfe1042d8a614c17b302f2f
David-FU019 <www.1227754540@qq.com>
2022-06-07 19:16:52 +0800

    添加注释

--- node-day2/02.创建web服务器.js
+++ node-day2/02.创建web服务器.js
@@ -4,8 +4,8 @@
 //4.开启服务器 server.listen
 
 const http = require('http')  //1.导入http 模块
-const server = http.createServer()
-server.on('request', (req, res) => {
+const server = http.createServer()  //2.创建服务器
+server.on('request', (req, res) => {   //3.设定服务器的监听require事件
     console.log('监听request事件');
     const str = `目前的地址是${req.url},请求方式是${req.method}`
     res.setHeader('Content-Type', 'text/html; charset=utf-8')
@@ -15,5 +15,5 @@ server.on('request', (req, res) => {
 
 
 server.listen(80, () => {
-    console.log('web服务器开启');
+    console.log('web服务器开启');  //4.开启服务器 server.listen
 })
